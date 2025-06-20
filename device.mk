5e2e81bb908d2a72888995daaf3733fae549f889
Tuan Anh <tuan73176@gmail.com>
2025-06-20 09:37:36 +0700

    caymanlm: Move all configuration files to `configs` folder
    Change-Id: I88146a5736ab6acf8d82f44e062c15911fe8d749

--- device.mk
+++ device.mk
@@ -13,10 +13,7 @@ $(call inherit-product, device/lge/sm7250-common/sm7250-common.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 # Fingerprint
 PRODUCT_PACKAGES += \
